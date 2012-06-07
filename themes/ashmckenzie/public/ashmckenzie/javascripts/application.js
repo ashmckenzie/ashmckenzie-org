1b683d537f129146a0be54af79a047e5104d924d
Ash McKenzie <ash@greenworm.com.au>
2012-06-07 23:08:46 +1000

    fixed comments, added head.js

--- themes/ashmckenzie/public/ashmckenzie/javascripts/application.js
+++ themes/ashmckenzie/public/ashmckenzie/javascripts/application.js
@@ -1,16 +1,6 @@
-$(document).ready(function() {
+head.ready(function() {
   
-  // Add a description <p> tag under each image and add title to <a> tag so
-  // Colorbox can utilise it
-  //
-  // $('p.image').each(function() { 
-  //   var a = $(this).find('a');
-  //   var img = $(this).find('img');
-  //   $(a).attr('title', $(img).attr('title'));
-  //   $('<p class="title">' + $(img).attr('title') + '</p>').insertAfter($(img));
-  // })
-
   // Colorbox lightbox for images
   //
-  $('article').each(function() { $(this).find('p.image a').colorbox(); })
+  $('article').each(function() { $(this).find('p.image a, a.cb-image').colorbox(); })
 });
