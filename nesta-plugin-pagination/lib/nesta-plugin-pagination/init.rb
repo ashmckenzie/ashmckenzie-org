69f6dbf5eea22e2d0996ab5546f9f88ddd085110
Ash McKenzie <ash@greenworm.com.au>
2012-08-27 22:59:36 +1000

    disable fancy pagination for now..

--- nesta-plugin-pagination/lib/nesta-plugin-pagination/init.rb
+++ nesta-plugin-pagination/lib/nesta-plugin-pagination/init.rb
@@ -7,12 +7,13 @@ module Nesta
         def paginate items
           page_param_name = :page
           if links = will_paginate(
-            items, 
-            previous_label: 'Newer', 
-            next_label: 'Older',
-            param_name: page_param_name
-          )
-            links.gsub(Regexp.new(/(?:\?|&)(#{page_param_name})=(\d+)/), '/\1/\2')
+              items,
+              previous_label: 'Newer',
+              next_label: 'Older',
+              param_name: page_param_name
+            )
+            # links.gsub(Regexp.new(/(?:\?|&)(#{page_param_name})=(\d+)/), '/\1/\2')
+            links
           else
             ''
           end
